08121506336b6b1f18d6ebf9e61e83e04aef2fcf
Ismail-Taha <taha.ismaile2002@gmail.com>
2023-06-15 14:21:13 -0700

    write_string

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,9 @@
+#include <unistd.h>
+/**
+ * Write the desired string to the standard error
+ * Return 1 to indicate an error
+ */
+int main(void) {
+	    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	        return 1;
+}
