69b571424081146f9ce6d16d1f1b80a4441f94ca
Ismail-Taha <taha.ismaile2002@gmail.com>
2023-06-15 14:31:46 -0700

    Fixee write_string

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,14 @@
 #include <unistd.h>
 /**
- * Write the desired string to the standard error
- * Return 1 to indicate an error
+ * main - Entry point of the program
+ * This program writes a specific string to the standard error.
+ * It returns an error code of 1 to indicate failure.
+ * Return: Always 1 (indicating an error)
  */
 int main(void) {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+     /* Write the desired string to the standard error */
+     write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+     return (1);
 }
+
