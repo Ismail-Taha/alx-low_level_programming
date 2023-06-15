9f40d4e0a6641b254ba5a21b19b2af1387e3ffcc
Ismail-Taha <taha.ismaile2002@gmail.com>
2023-06-15 16:14:11 -0700

    the last fix 101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,12 @@
 #include <unistd.h>
+
 /**
- * main - Entry point of the program
- * This program writes a specific string to the standard error.
- * It returns an error code of 1 to indicate failure.
- * Return: Always 1 indicating an error
- */
-int main(void) 
+ *  * main - prints with write function
+ *   * Return: 0 if exited properly, non-zero otherwise
+ *    */
+
+int main(void)
 {
-     /* Write the desired string to the standard error */
      write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
      return (1);
 }
-
