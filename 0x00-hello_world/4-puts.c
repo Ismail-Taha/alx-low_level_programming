baaa5dcdda68cfd1951701a8c5b68a3251e28a90
Ismail-Taha <taha.ismaile2002@gmail.com>
2023-06-15 14:01:09 -0700

    fix Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-
 /**
- *  * main - Entry point
- *   * learning puts function
- *    * Return: Alwys 0 (success)
- *     */
+ * main - Entry point
+ *  learning puts function
+ * Return: Always 0 (success)
+ */
+
 int main(void)
 {
 		char tx[] = "\"Programming is like building a multilingual puzzle";
