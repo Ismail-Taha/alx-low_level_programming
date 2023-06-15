623eaad2022604d9dfb2134931866095f5bcec0b
Ismail-Taha <taha.ismaile2002@gmail.com>
2023-06-15 14:44:52 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,12 @@
  * main - Entry point of the program
  * This program writes a specific string to the standard error.
  * It returns an error code of 1 to indicate failure.
- * Return: Always 1 (indicating an error)
+ * Return: Always 1 indicating an error
  */
-int main(void) {
+int main(void) 
+{
      /* Write the desired string to the standard error */
      write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-
      return (1);
 }
 
