b2ec5c40bf2febd2629f2b1ca6f765f2f2dabadb
Ismail-Taha <taha.ismaile2002@gmail.com>
2023-08-03 13:41:04 -0700

    hf

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,12 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/*
+ * Description: Header file containing prototypes for all Functions
+ *       written in the 0x14-bit_manipulation Directory
+ */
+
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+
+#endif
