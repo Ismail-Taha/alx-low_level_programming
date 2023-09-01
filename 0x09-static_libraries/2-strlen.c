e70ca2d1feadc9f2183d45b19b6841d7b633c415
Ismail-Taha <tahaismail.allali@gmail.com>
2023-09-01 14:11:46 -0700

    gg

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - function that calculate the lenght of str
+ * @s: pointer to the str
+ * Return: returns the lenght of str
+ */
+int _strlen(char *s)
+{
+int i;
+char b;
+for (i = 0; b != '\0'; i++)
+{
+b = s[i];
+}
+return (i - 1);
+}
---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,1 @@
+echo readme for static librairies
