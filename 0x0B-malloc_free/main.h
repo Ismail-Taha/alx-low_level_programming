1d04257233d6599e5be3f7b8830fd105f1269ad0
Ismail-Taha <tahaismail.allali@gmail.com>
2023-09-05 15:49:07 -0700

    gg joe and isma

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -12,4 +12,8 @@ int strleng(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
+int word_len(char *str);
+int count_words(char *str);
+char **strtow(char *str);
+char *argstostr(int ac, char **av);
 #endif /* MAIN_H */
