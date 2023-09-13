923556a1c19acf6626002568fad7da07b1ec4f12
Ismail-Taha <tahaismail.allali@gmail.com>
2023-09-13 14:28:13 -0700

    fun-print

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+/**
+ * print_name - function that Prints a Name.
+ * @name: The name to be printed.
+ * @f: A pointer to a Function that prints a Name.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return null;
+
+	f(name);
+}
