7877cee076b61f09e9cb1d34c9b41673950b3fae
Ismail-Taha <tahaismail.allali@gmail.com>
2023-09-21 17:09:41 -0700

    last!

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,17 @@
+section .data
+	format db "Hello, Holberton" ,0
+	new_line db 10,0
+
+section .txt
+	global main
+
+main:
+	mov rdi,format
+	call printf
+
+	mov rdi, format
+	call printf
+
+	mov rax, 60
+	xor rdi, rdi
+	syscall
