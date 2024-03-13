0491c219574d9f7d61521b6e5b3bbd7d4743b618
Ismail-Taha <taha.ismaile2002@gmail.com>
2024-03-13 08:32:27 -0700

    fix and test

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,64 +1,36 @@
 #include "search_algos.h"
 
 /**
- * binary_search - search a value in to ordered array
- * @array: integer array
- * @size: number of elements in array
- * @value: the value to search
- * Return: the first index with value matched or -1 otherwise
-*/
-
+  * binary_search - function searches for a value in a sorted array
+  *                 of integers using binary search.
+  * @array: A pointer to the first element of the array to search.
+  * @size: The number of elements in the array.
+  * @value: The value to search for.
+  *
+  * Return: index where value is located.
+  */
 int binary_search(int *array, size_t size, int value)
 {
-	size_t star, end;
-	int i;
+	size_t i, star, last;
 
 	if (array == NULL)
 		return (-1);
-	star = 0;
-	end = size - 1;
-	i = (star + end) / 2;
-	while (star <= end)
+
+	for (star = 0, last = size - 1; last >= star;)
 	{
-		print_array(array, star, end);
-		i = (star + end) / 2;
+		printf("Searching in array: ");
+		for (i = star; i < last; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[i]);
+
+		i = star + (last - star) / 2;
 		if (array[i] == value)
 			return (i);
-		else if (array[i] > value)
-		{
-			end = i - 1;
-		}
+		if (array[i] > value)
+			last = i - 1;
 		else
-		{
 			star = i + 1;
-		}
 	}
+
 	return (-1);
 }
-
-/**
- * print_array - search a value in to ordered array
- * @array: integer array
- * @start: initial index to print
- * @end: end index to print
-*/
-
-void print_array(int *array, size_t start, size_t end)
-{
-	size_t i, is_fir = 0;
-
-	printf("Searching in array: ");
-
-	for (i = start; i <= end; i++)
-	{
-		if (is_fir == 0)
-		{
-			printf("%d", array[i]);
-			is_fir = 1;
-		}
-		else
-			printf(", %d", array[i]);
-	}
-	printf("\n");
-}
-
